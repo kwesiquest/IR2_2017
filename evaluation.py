878b721295175e69dd9b568b628c44e1039b7c99
Kasper Bouwens <kas_bouwens@hotmail.com>
2017-11-22 12:11:03 +0100

    evaluation metrics initial commit

---
+++ evaluation.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class NDCG_at_k:
+    
+    def __init__(self, k=10):
+        max_relevance = [1] + [0] * k
+        self.normalizing_factor = self.NDCG(max_relevance, k, norm=False) 
+        
+    def NDCG(self, ranking, k, norm):    
+        discounted_gain = []
+    
+        for rank, relevance in enumerate(ranking[0:k]):
+            discounted_gain.append(((2.**relevance)-1)/float(np.log2(rank + 2)))
+        
+        DG = sum(discounted_gain)
+        if norm:
+            return DG/self.normalizing_factor
+        else: return DG
+
+class precision_at_k:
+    def __init__(self, k=10):
+        self.k = k
+    
+    def precision(self, ranking):
+        return np.float(np.sum(ranking))/self.k
+
