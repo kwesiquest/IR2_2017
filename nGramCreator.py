53f463148aa92480627479caf05b5316c9a4846c
RoderickvanderWeerdt <rvanderweerdt@hotmail.com>
2017-11-28 14:03:34 +0100

    nGramParser yields ngrams

--- nGramCreator.py
+++ nGramCreator.py
@@ -12,13 +12,17 @@ def create_nGrams(n, text):
 def create_ID_nGrams(n, pickle_file):
 	productList = pd.read_pickle(pickle_file)
 	id_nGrams = []
+	size = 0
 	for prod_id, reviewText in zip(productList["prod_id"], productList["reviewText"]):
 		id_nGrams.append([prod_id, create_nGrams(n, reviewText)])
+		size += 1
+		if size >=128:
+			break
 	return pd.DataFrame(id_nGrams, columns=["prod_id", "reviewText"])
 
 n = 3
 pickle_file = "reviews_Home_and_Kitchen_5.pickle"
-new_file = "reviews_Home_and_Kitchen_" + str(n) + "grams.pickle"
+new_file = "reviews_Home_and_Kitchen_" + str(n) + "grams_small.pickle"
 
 df = create_ID_nGrams(n, pickle_file)
 
---
+++ nGramParser.py
@@ -0,0 +1,40 @@
+#based on code from: http://jmcauley.ucsd.edu/data/amazon/links.html
+
+import pandas as pd
+import gzip
+import numpy as np
+
+def parse(path):
+	g = gzip.open(path, 'rb')
+	for l in g:
+		yield eval(l)
+
+def parse_nGrams(path, n):
+	g = gzip.open(path, 'rb')
+	for l in g:
+		data = eval(l)
+		for nGram in create_nGrams(data['reviewText'], n):
+			yield nGram
+
+def create_nGrams(text, n):
+	nGrams = []
+	words = text.split()
+	i = 0
+	while i+n <= len(words):
+		nGrams.append(words[i:i+n])
+		i += 1
+	return nGrams
+
+if __name__ == '__main__':
+	path = "reviews_Home_and_Kitchen_5.json.gz"
+	n = 4
+
+	i = 0
+	for nGram in parse_nGrams(path, n):
+		print(nGram)
+		if i >= 500:
+			break
+		else:
+			i+= 1
+
+	print(i, str(n) + "-grams processed.")
