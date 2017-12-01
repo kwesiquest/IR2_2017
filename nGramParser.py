039732c8ef4554f846b60fb52d18b55d275c5b8a
RoderickvanderWeerdt <rvanderweerdt@hotmail.com>
2017-12-01 17:05:26 +0100

    created useful EntityDict class

--- nGramParser.py
+++ nGramParser.py
@@ -1,37 +1,45 @@
-#based on code from: http://jmcauley.ucsd.edu/data/amazon/links.html
+### use this file by first creating an object "EntityDict(<str: pathToReview.json.gz>, <int: nGram size>)"
+###
+### then call the function "next_entity(<int: number of dissimilar entities>)" in a loop, which yields an 
+### "returnPackage()" object, consisting of the entity's id (".entity_id"), its documents in a list (".docs")
+### and a list of documents belonging to z different entities (".dissimilars") 
 
-import pandas as pd
 import gzip
 import numpy as np
 import argparse
 
-from os.path import isfile
+class returnPackage:
+	#docs = docs * nGrams * words
+	#dissimilars = docs * nGrams * words
+	def __init__(self):
+		self.entity_id = ""
+		self.docs = []
+		self.dissimilars = []
 
-class doc_parser:
-	def __init__(self, path, n):
+class EntityDict:
+	# creates a dictionary with entities as keys and all the corresponding documents (in  a list) as values
+	# a documents is a list of ngrams
+	# ngrams are a list of strings
+	def __init__(self, path, n, limited=False):
+		# self.entity_pointer = 0
 		self.path = path
 		self.n = n
-		self.entity_docs = {}
-		if isfile(path+".csv"):
-			print("found existing .csv")
-			pd.read_csv(path+".csv")
-		else:
-			print("creating new dictionary")
-			for doc in self.parse():
-				nGrams = create_nGrams(doc['reviewText'], self.n)
-				entity_id = doc['asin']
-				try:
-					self.entity_docs[entity_id].append(nGrams)
-				except:
-					self.entity_docs[entity_id] = [nGrams]
-			print("created entity dictionary.")
-			entity_data = pd.DataFrame(columns=["prod_id", "docs"])
-			entity_data["prod_id"] = self.entity_docs.keys()
-			entity_data["docs"] = self.entity_docs.values()
-			entity_data.to_csv(path + ".csv")
+		self.entity_dict = {}
+		self.continuous = True
+		print("creating new dictionary...")
+		for doc in self.parse():
+			nGrams = create_nGrams(doc['reviewText'], self.n)
+			entity_id = doc['asin']
+			try:
+				self.entity_dict[entity_id].append(nGrams)
+			except:
+				self.entity_dict[entity_id] = [nGrams]
+			if limited and (len(self.entity_dict.keys()) >= 4):
+				break
+		self.entities = list(self.entity_dict.keys())
+		print("created entity dictionary containing", len(self.entities), "entities.")
 
-		# self.n_entities = len(self.entity_docs.keys())
-
+	#based on code from: http://jmcauley.ucsd.edu/data/amazon/links.html
 	def parse(self):
 		g = gzip.open(self.path, 'rb')
 		for l in g:
@@ -41,28 +49,39 @@ class doc_parser:
 		g = gzip.open(self.path, 'rb')
 		different_id = entity_id
 		while(entity_id == different_id):
-			different_id = np.random.choice(self.entity_docs.keys())
+			different_id = np.random.choice(self.entities)
 		return different_id
 
+	def get_dissimilars(self, entity_id, z):
+		dissimilars = []
+		for _ in range(0, z):
+			dissimilars += self.entity_dict[self.get_random_id(entity_id)] #dissimilars is a list of documents
+			# return_value.dissimilars.append(self.entity_dict[self.get_random_id(return_value.entity_id)]) #dissimilars is a list of entities
+		return dissimilars
+
+	# def next_entity_id(self):
+	# 	next_id = self.entities[self.entity_pointer]
+	# 	self.entity_pointer += 1
+	# 	if(self.entity_pointer >= len(self.entities)):
+	# 		self.entity_pointer = 0
+	# 	return next_id
+
+	# # z = number of non-similar entities
+	# def next_entity_continuous(self, z):
+	# 	return_value = returnPackage()
+	# 	return_value.entity_id = self.next_entity_id()
+	# 	return_value.docs = self.entity_dict[return_value.entity_id]
+	# 	return_value.dissimilars = self.get_dissimilars(entity_id, z)
+	# 	return return_value
+
 	# z = number of non-similar entities
-	# def parse_batch(self, z):
-		# for entity_id in self.
-
-
-
-	# def parse_nGrams(self, n):
-	# 	g = gzip.open(self.path, 'rb')
-	# 	for l in g:
-	# 		data = eval(l)
-	# 		for nGram in create_nGrams(data['reviewText'], n):
-	# 			yield nGram
-
-	# def parse_batch(self, n):
-	# 	g = gzip.open(self.path, 'rb')
-	# 	for l in g:
-	# 		data = eval(l)
-	# 		nGrams = create_nGrams(data['reviewText'], n)
-	# 		yield {'entity_id':data['asin'], 'nGrams':nGrams, 'dissimilars':}
+	def next_entity(self, z):
+		for entity_id in self.entities:
+			return_value = returnPackage()
+			return_value.entity_id = entity_id
+			return_value.docs = self.entity_dict[entity_id]
+			return_value.dissimilars = self.get_dissimilars(entity_id, z)
+			yield return_value
 
 def create_nGrams(text, n):
 	nGrams = []
@@ -73,26 +92,19 @@ def create_nGrams(text, n):
 		i += 1
 	return nGrams
 
-if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Process some integers.')
-	parser.add_argument('-path', type=str, help='path to data', default="reviews_Home_and_Kitchen_5.json.gz" )
-	parser.add_argument('-n', type=int, help='specify n for ngrams', default=4)
+# if __name__ == '__main__':
+# 	parser = argparse.ArgumentParser(description='Process some integers.')
+# 	parser.add_argument('-path', type=str, help='path to data', default="reviews_Home_and_Kitchen_5.json.gz" )
+# 	parser.add_argument('-n', type=int, help='specify n for ngrams', default=4)
 
 	 
-	FLAGS, unparsed = parser.parse_known_args()
-	path = FLAGS.path
-	n = FLAGS.n
-     
-	# i = 0
-	# for nGram in parse_nGrams(path, n):
-	# 	print(nGram)
-	# 	if i >= 500:
-	# 		break
-	# 	else:
-	# 		i+= 1
+# 	FLAGS, unparsed = parser.parse_known_args()
+# 	path = FLAGS.path
+# 	n = FLAGS.n
 
-	parser = doc_parser(path, n)
+# 	parser = EntityDict(path, n, True)
 
-
-
-	# print(i, str(n) + "-grams processed.")
+# 	i = 0
+# 	for entity in parser.next_entity(2):
+# 		print(i)
+# 		i+= 1
