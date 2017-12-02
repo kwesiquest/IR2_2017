e41c08a32e9ecf0f58f8ad9937299c9e5be626b7
robinderat <robin-1194@hotmail.com>
2017-12-02 16:27:45 +0100

    finished LSE
    loss goes down for hardcoded example
    
    just run LSE_train.py

---
+++ LSE_train.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 12:59:14 2017
+
+@author: Robin Bakker
+"""
+
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+import nGramParser as gram
+
+from LSE import LSE as LSE
+
+BATCH_SIZE = None
+W_SIZE = 100
+E_SIZE = 150
+LEARNING_RATE = 1e-3
+PATH_TO_DATA = 'D:\\UvA\\IR2\\'
+TRAIN_STEPS = 100
+N_GRAM_SIZE = 1
+DISSIMILAR_AMOUNT = 10
+
+vocabulary = ['a','b','c','d']
+vocab_size = len(vocabulary)
+
+print('Loading data')
+print('Preprocessing')
+print('Finished vocabulary')
+
+ngrams = [['a'],['b'],['-']]
+docs = np.array([[['a'],['b'],['-']],[['c'],['d'],['a']]])
+dissimilars = [[[['a'],['b'],['-']],[['c'],['d'],['a']]]]
+entity_amount = 1
+
+model = LSE(BATCH_SIZE,W_SIZE,E_SIZE,vocab_size,vocabulary,entity_amount, LEARNING_RATE)
+
+#string_placeholder = tf.placeholder(tf.string)
+#one_hot_avg_placeholder = tf.placeholder(tf.float32,shape=(None))
+#matching_placeholder = tf.placeholder(tf.float32,shape=(1,E_SIZE))
+#others_placeholder = tf.placeholder(tf.float32,shape=(None,E_SIZE))
+
+#one_hot = model.getOneHot(string_placeholder)
+#word_emb = model.getWordEmbedding(one_hot_avg_placeholder)
+#predicted = model.getEntityEmbedding(word_emb)
+#similar = model.getSimilarity(matching_placeholder, predicted)
+#dissimilar = model.getSimilarity(others_placeholder,predicted)
+#probability = model.getProbability(similar,dissimilar)
+#loss = model.loss(probability)
+ngrams_placeholder = tf.placeholder(tf.string, shape=(None,N_GRAM_SIZE))
+documents_placeholder = tf.placeholder(tf.string, shape=(None,None,N_GRAM_SIZE))
+dissimilar_placeholder = tf.placeholder(tf.float32, shape=(E_SIZE, None))
+
+entity = model.entityEmbedding(documents_placeholder)
+projection = model.project(ngrams_placeholder)
+similarity = model.similarity(entity, dissimilar_placeholder, projection)
+loss = model.loss(similarity)
+train_step = model.train_step(loss)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    tf.tables_initializer().run()
+    
+    for i in range(TRAIN_STEPS):
+        print(i)
+    
+    #    ngrams, similar, dissimilar = data.getNgrams(path,N_GRAM_SIZE)
+    
+        ngrams_feed = {ngrams_placeholder: ngrams}
+        ngrams_emb = sess.run(projection,feed_dict = ngrams_feed) 
+    
+        similar = docs
+        similar_feed = {documents_placeholder: similar}
+        similar_emb = sess.run(entity,feed_dict = similar_feed)
+        
+        diss = None
+        for dissimilar in dissimilars:
+            dissimilar_feed = {documents_placeholder: dissimilar}
+            dissimilar_emb = sess.run(entity,feed_dict = dissimilar_feed)
+            dissimilar_emb = np.expand_dims(dissimilar_emb,1)
+            if diss == None:
+                diss = dissimilar_emb
+            else:
+                diss = np.stack(diss,dissimilar_emb, 1)
+                
+        print(sess.run(loss, feed_dict={ngrams_placeholder: ngrams, documents_placeholder: docs, dissimilar_placeholder: diss}))
+        
+        sess.run(train_step, feed_dict={ngrams_placeholder: ngrams, documents_placeholder: docs, dissimilar_placeholder: diss})
+        
+    
+    
